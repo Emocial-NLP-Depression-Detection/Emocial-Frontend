a8c0dfb7f71757b85b170adf773b52297e694cb4
SiravitPhokeed <siravitsasa@gmail.com>
2021-04-17 11:22:04 +0700

    New blank Compose page

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 // ReactJS import
-import React from "react";
+import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 // All pages import
-import { WelcomeCover } from './script/welcome.js'
-import { Home } from './script/home.js'
-import { Select } from './script/select.js'
+import { WelcomeCover } from './script/welcome.js';
+import { Home } from './script/home.js';
+import { Select } from './script/select.js';
+import { Compose } from './script/compose.js';
 import { History } from './script/history.js';
 import { HandleDetails } from './script/history_details.js';
 import { NotFound } from './script/not_found.js';
@@ -18,6 +19,7 @@ function App() {
             <Route exact path="/" component={WelcomeCover} />
             <Route exact path="/home" component={Home} />
             <Route exact path="/select" component={Select} />
+            <Route exact path="/compose" component={Compose} />
             <Route exact path="/history" component={History} />
             <Route path="/history/" component={HandleDetails} />
             <Route path="/" component={NotFound} />
