89ea6886870bf9bebc33754802d34c92d5cde137
SiravitPhokeed <siravitsasa@gmail.com>
2021-05-06 17:02:39 +0700

    Added Axios to dependencies

---
+++ src/script/debug.js
@@ -0,0 +1,14 @@
+// Temporary page for integration
+import React from 'react';
+
+class Debug extends React.Component {
+    render() {
+        return (
+            <div>
+                hello
+            </div>
+        );
+    }
+}
+
+export { Debug }
