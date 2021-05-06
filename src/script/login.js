6379762e436807cad6d64d9cb17ef1e2cb2c1a32
SiravitPhokeed <siravitsasa@gmail.com>
2021-05-06 23:52:32 +0700

    New login page for real this time

---
+++ src/script/login.js
@@ -0,0 +1,49 @@
+// ReactJS import
+import React from 'react';
+
+// Style sheet import
+import '../css/global.css'
+import '../css/account.css';
+import '../css/login.css';
+
+// Menu bar import
+import { Menu } from './menu.js'
+
+// Translation keys import
+import { checkLanguage, translation } from './translation.js';
+
+class Form extends React.Component {
+    constructor(props) {
+        super(props);
+        this.language = this.props.language
+    }
+
+    render() {
+        return (
+            <div>
+                oh hi
+            </div>
+        );
+    }
+}
+
+class Login extends React.Component {
+    constructor(props) {
+        super(props);
+        this.language = checkLanguage();
+    }
+
+    render() {
+        return (
+            <div>
+                <div class="slide-in-onload">
+                    <Menu path="/settings" />
+                </div>
+                <div class="fade-in-onload">
+                    <Form />
+                    Don't have an account? Sign up
+                </div>
+            </div>
+        );
+    }
+}
