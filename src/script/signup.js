3810b3dcd00263d78ad8d7d82e31d12686fe6591
SiravitPhokeed <siravitsasa@gmail.com>
2021-05-07 21:16:47 +0700

    Complete Sign up page I think

--- src/script/signup.js
+++ src/script/signup.js
@@ -12,6 +12,10 @@ import { Menu } from './menu.js'
 // Translation keys import
 import { checkLanguage, translation } from './translation.js';
 
+// Pictures import
+import doctor from '../photos/type_doctor.png'
+import patient from '../photos/type_patient.png'
+
 class Form extends React.Component {
     constructor(props) {
         super(props);
@@ -34,6 +38,39 @@ class Form extends React.Component {
     }
 }
 
+class ChooseType extends React.Component {
+    constructor(props) {
+        super(props);
+        this.lang = this.props.lang;
+    }
+
+    render() {
+        return (
+            <div className="choose-type-grid">
+                <div>
+                    <p className="choose-type-title">{translation.settings.account.type.title[this.lang]}</p>
+                    <button className="choose-type-button will-animate">
+                        <div className="choose-type-img-container">
+                            <img className="choose-type-img" src={doctor} alt="Doctor" />
+                        </div>
+                        <span className="choose-type-text">{translation.settings.account.type.doctor[this.lang]}</span>
+                    </button>
+                    <button className="choose-type-button will-animate">
+                        <div className="choose-type-img-container">
+                            <img className="choose-type-img" src={patient} alt="Nondescript male person" />
+                        </div>
+                        <span className="choose-type-text">{translation.settings.account.type.patient[this.lang]}</span>
+                    </button>
+                </div>
+                <div className="account-submit-container">
+                    <button className="account-submit will-animate">{translation.account.signup_submit[this.lang]}</button>
+                </div>
+            </div>
+
+        );
+    }
+}
+
 class SignUp extends React.Component {
     constructor(props) {
         super(props);
@@ -46,8 +83,20 @@ class SignUp extends React.Component {
                 <div className="slide-in-onload">
                     <Menu />
                 </div>
-                <div className="fade-in-onload">
-
+                <div className="fade-in-onload signup-flex">
+                    <div>
+                        <h1 className="account-header">{translation.account.signup_title[this.lang]}</h1>
+                        <div className="signup-grid">
+                            <Form lang={this.lang} />
+                            <ChooseType lang={this.lang} />
+                        </div>
+                        <div className="bottom-text signup-bottom-text">
+                            {translation.account.signup_bottom[this.lang]}
+                            <span className="account-link will-animate">
+                                {translation.account.login_submit[this.lang]}
+                            </span>
+                        </div>
+                    </div>
                 </div>
             </div>
         );
