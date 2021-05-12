857029564dc625c0402aadbaba8e9d6ae7b04250
SiravitPhokeed <siravitsasa@gmail.com>
2021-05-12 10:48:30 +0700

    Fixed #28 for real this time

--- src/script/questionnaire.js
+++ src/script/questionnaire.js
@@ -17,52 +17,54 @@ class Questionnaire extends React.Component {
     constructor(props) {
         super(props);
         this.lang = checkLanguage();
-        this.state = { message1: null, message2: null };
+        this.state = { q1: null, q2: null, to_post: null };
     }
 
     handleClick() {
-        console.log(this.state);
+        this.setState({ to_post: { "q1": this.state.q2, "q2": this.state.q2, "lang": this.lang } })
     }
 
     render() {
-        return (
-            <div className="questionnaire-root">
-                <div className="slide-in-onload">
-                    <Menu />
-                </div>
-                <div className="fade-in-onload">
-                    <div className="questionnaire">
-                        <div className="question">
-                            <p className="question-number">1</p>
-                            <div className="question-content">
-                                <label className="question-label" htmlFor="composed-tweet">
-                                    {translation.questionnaire.one[this.lang]}
-                                </label><br />
-                                <textarea className="compose-textarea will-animate" id="question-1" name="question-1" onChange={e => this.setState({ message1: e.target.value })} autoFocus={true} /><br />
-                                <Link to="#2" className="question-next compose-submit will-animate">
-                                    {translation.questionnaire.next[this.lang]}
-                                </Link>
+        if (this.state.to_post) {
+            return (<Result post_type="questionnaire" to_post={this.state.to_post} />);
+        } else {
+            return (
+                <div className="questionnaire-root">
+                    <div className="slide-in-onload">
+                        <Menu />
+                    </div>
+                    <div className="fade-in-onload">
+                        <div className="questionnaire">
+                            <div className="question">
+                                <p className="question-number">1</p>
+                                <div className="question-content">
+                                    <label className="question-label" htmlFor="composed-tweet">
+                                        {translation.questionnaire.one[this.lang]}
+                                    </label><br />
+                                    <textarea className="compose-textarea will-animate" id="question-1" name="question-1" onChange={e => this.setState({ message1: e.target.value })} autoFocus={true} /><br />
+                                    <Link to="#2" className="question-next compose-submit will-animate">
+                                        {translation.questionnaire.next[this.lang]}
+                                    </Link>
+                                </div>
                             </div>
-                        </div>
-                        <div className="question" id="2">
-                            <p className="question-number">2</p>
-                            <div className="question-content">
-                                <label className="question-label" htmlFor="composed-tweet">
-                                    {translation.questionnaire.two[this.lang]}
-                                </label><br />
-                                <textarea className="compose-textarea will-animate" id="question-2" name="question-2" onChange={e => this.setState({ message2: e.target.value })} /><br />
-                                <button className="compose-submit will-animate" onClick={() => this.handleClick()}>
-                                    {translation.questionnaire.submit[this.lang]}
-                                </button>
+                            <div className="question" id="2">
+                                <p className="question-number">2</p>
+                                <div className="question-content">
+                                    <label className="question-label" htmlFor="composed-tweet">
+                                        {translation.questionnaire.two[this.lang]}
+                                    </label><br />
+                                    <textarea className="compose-textarea will-animate" id="question-2" name="question-2" onChange={e => this.setState({ message2: e.target.value })} /><br />
+                                    <button className="compose-submit will-animate" onClick={() => this.handleClick()}>
+                                        {translation.questionnaire.submit[this.lang]}
+                                    </button>
+                                </div>
                             </div>
                         </div>
                     </div>
                 </div>
-            </div>
-
-        );
+            );
+        }
     }
-
 }
 
 export { Questionnaire }
