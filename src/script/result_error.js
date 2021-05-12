b540d54c293d282fe155027136c98f22dd156e42
SiravitPhokeed <siravitsasa@gmail.com>
2021-05-12 11:00:52 +0700

    New error page woo

---
+++ src/script/result_error.js
@@ -0,0 +1,13 @@
+// ReactJS import
+import React from 'react';
+
+// Style sheet import
+import '../css/global.css'
+import '../css/result.css';
+import '../css/error.css';
+
+// Menu bar import
+import { Menu } from './menu.js'
+
+// Translation keys import
+import { checkLanguage, translation } from './translation.js';
