73cf9f1f4135030d2bb663864598000a16dc5ab7
SiravitPhokeed <siravitsasa@gmail.com>
2021-05-14 01:03:06 +0700

    Fully working signup I'll go to sleep now

--- src/script/signup.js
+++ src/script/signup.js
@@ -1,5 +1,7 @@
 // ReactJS import
 import React from 'react';
+import { Redirect } from 'react-router-dom';
+import axios from 'axios';
 
 // Style sheet import
 import '../css/global.css'
@@ -49,48 +51,67 @@ class ChooseType extends React.Component {
     constructor(props) {
         super(props);
         this.lang = this.props.lang;
+        this.input_obj = null;
+        this.to_post = null;
         this.doctor_ref = React.createRef();
         this.patient_ref = React.createRef();
+        this.state = { is_doctor: false, redirect: null };
     }
 
     chooseType(type) {
         if (type === "doctor") {
+            this.setState({ is_doctor: true });
             this.doctor_ref.current.classList.add("secondary-light-active");
             this.patient_ref.current.classList.remove("secondary-light-active");
         } else if (type === "patient") {
+            this.setState({ is_doctor: false });
             this.patient_ref.current.classList.add("secondary-light-active");
             this.doctor_ref.current.classList.remove("secondary-light-active");
         }
     }
 
     handleClick() {
-        console.log(input_obj);
+        this.input_obj = input_obj;
+        this.to_post = {
+            "username": this.input_obj.name,
+            "email": this.input_obj.email,
+            "password": this.input_obj.password,
+            "twitterAcount": this.input_obj.handle,
+            "status": this.state.is_doctor
+        }
+        console.log("POST", this.to_post, "to /register")
+        axios.post("http://localhost:8000/register", this.to_post)
+            .then((res) => console.log("Server responded with", res.status, res.statusText));
+        this.setState({ redirect: "/settings" })
     }
 
     render() {
-        return (
-            <div className="choose-type-grid">
-                <div>
-                    <p className="choose-type-title">{translation.settings.account.type.title[this.lang]}</p>
-                    <button ref={this.doctor_ref} className="choose-type-button secondary-light will-animate" onClick={() => this.chooseType("doctor")}>
-                        <div className="choose-type-img-container">
-                            <img className="choose-type-img" src={doctor} alt="Doctor" />
-                        </div>
-                        <span className="choose-type-text">{translation.settings.account.type.doctor[this.lang]}</span>
-                    </button>
-                    <button ref={this.patient_ref} className="choose-type-button secondary-light will-animate" onClick={() => this.chooseType("patient")}>
-                        <div className="choose-type-img-container">
-                            <img className="choose-type-img" src={patient} alt="Nondescript male person" />
-                        </div>
-                        <span className="choose-type-text">{translation.settings.account.type.patient[this.lang]}</span>
-                    </button>
+        if (this.state.redirect) {
+            return <Redirect to={this.state.redirect} />
+        } else {
+            return (
+                <div className="choose-type-grid">
+                    <div>
+                        <p className="choose-type-title">{translation.settings.account.type.title[this.lang]}</p>
+                        <button ref={this.doctor_ref} className="choose-type-button secondary-light will-animate" onClick={() => this.chooseType("doctor")}>
+                            <div className="choose-type-img-container">
+                                <img className="choose-type-img" src={doctor} alt="Doctor" />
+                            </div>
+                            <span className="choose-type-text">{translation.settings.account.type.doctor[this.lang]}</span>
+                        </button>
+                        <button ref={this.patient_ref} className="choose-type-button secondary-light will-animate" onClick={() => this.chooseType("patient")}>
+                            <div className="choose-type-img-container">
+                                <img className="choose-type-img" src={patient} alt="Nondescript male person" />
+                            </div>
+                            <span className="choose-type-text">{translation.settings.account.type.patient[this.lang]}</span>
+                        </button>
+                    </div>
+                    <div className="account-submit-container">
+                        <button className="account-submit will-animate" onClick={() => this.handleClick()}>{translation.account.signup_submit[this.lang]}</button>
+                    </div>
                 </div>
-                <div className="account-submit-container">
-                    <button className="account-submit will-animate" onClick={() => this.handleClick()}>{translation.account.signup_submit[this.lang]}</button>
-                </div>
-            </div>
-
-        );
+            );
+        }
     }
 }
 
